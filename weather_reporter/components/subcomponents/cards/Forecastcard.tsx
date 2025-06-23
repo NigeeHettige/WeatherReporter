1f4d588a4436e7e070563fb136fb4ef6d522196d
Nigee Hettige <135543588+NigeeHettige@users.noreply.github.com>
2025-06-24 00:19:58 +0530

    resolve lint errors

--- weather_reporter/components/subcomponents/cards/Forecastcard.tsx
+++ weather_reporter/components/subcomponents/cards/Forecastcard.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import Hourforecastcard from "./Hourforecastcard.tsx";
 import Dayforecastcard from "./Dayforecastcard.tsx";
-
-const Forecastcard = React.memo(() => {
+import Hourforecastcard from "./Hourforecastcard.tsx";
+function Forecastcard() {
   return (
     <div className="flex flex-col gap-6">
       <Hourforecastcard />
       <Dayforecastcard />
     </div>
   );
-});
+}
 
 export default Forecastcard;
