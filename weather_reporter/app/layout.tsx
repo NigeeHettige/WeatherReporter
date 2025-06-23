9dc78b00ce0e9ca2f32bbfa1d9d81ce0cc03a82b
Nigee Hettige <135543588+NigeeHettige@users.noreply.github.com>
2025-06-23 23:30:36 +0530

    feat: add sitemap.xml, robots.txt, SEO keywords, and Open Graph metadata

--- weather_reporter/app/layout.tsx
+++ weather_reporter/app/layout.tsx
@@ -15,9 +15,33 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://weather-reporter-ten.vercel.app"),
+  keywords: [
+    "weather",
+    "weather tomorrow",
+    "weather today",
+    "weather forecast",
+    "forecast",
+  ],
   title: "Weather Report",
   description:
     "Weather Reporter is a real-time weather forecasting app that provides accurate current conditions, hourly updates, and 5-day forecasts using modern web technologies.",
+  openGraph: {
+    title: "Weather Reporter",
+    description:
+      "Weather Reporter is a real-time weather forecasting app that provides accurate current conditions, hourly updates, and 5-day forecasts using modern web technologies.",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Weather Reporter Open Graph Image",
+      },
+    ],
+    type: "website",
+    locale: "en_US",
+    siteName: "Weather Reporter",
+  },
 };
 
 export default function RootLayout({
